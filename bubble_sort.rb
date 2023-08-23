8370e7dff2d525082688b5d5ae6fe61d72b25020
Mon Inson <66734118+monaxx@users.noreply.github.com>
2023-08-23 17:14:24 +0800

    Perform bubble sort

---
+++ bubble_sort.rb
@@ -0,0 +1,16 @@
+def bubble_sort(arr_price)
+  for i in 0...arr_price.length
+    sliced_array = arr_price.slice(0, arr_price.length - i)
+    for j in 0...(sliced_array.length - 1)
+      if arr_price[j] > arr_price[j + 1]
+        temp = arr_price[j]
+        arr_price[j] = arr_price[j + 1]
+        arr_price[j + 1] = temp
+      end
+    end
+  end
+  
+  arr_price
+end
+
+p bubble_sort([4,3,78,2,0,2])
